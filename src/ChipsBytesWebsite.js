e144dcc2694cb292e866def16f56abc9ee80185b
K. Sharat <sharat45k@gmail.com>
2025-07-30 17:03:38 +0530

    nav

--- src/ChipsBytesWebsite.js
+++ src/ChipsBytesWebsite.js
@@ -1,19 +1,13 @@
 // Updated ChipsBytesWebsite.jsx with new tabs and no icons
 
-import React, { useState } from 'react';
-import Navbar from './components/Navbar/Navbar';
+import React from 'react';
 import HomePage from './components/Pages/HomePage';
 import './style.css';
 
-const ChipsBytesWebsite = () => {
-  const [activeTab, setActiveTab] = useState('home');
-
-  return (
-    <div className="app">
-      <Navbar activeTab={activeTab} setActiveTab={setActiveTab} />
-      <HomePage />
-    </div>
-  );
-};
+const ChipsBytesWebsite = () => (
+  <div className="app">
+    <HomePage />
+  </div>
+);
 
 export default ChipsBytesWebsite;
